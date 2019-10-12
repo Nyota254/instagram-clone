e9dd76b2d37eb9f91bf8d412b2890683f41af0db
Nyota254 <knyota66@gmail.com>
2019-10-12 12:28:26 +0300

    add bio to profile model

--- users/models.py
+++ users/models.py
@@ -7,7 +7,8 @@ class Profile(models.Model):
     Creates fields that will be needed for the user profile
     '''
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic = models.ImageField(default='default.jpg',upload_to='profile_pics') 
+    profile_pic = models.ImageField(default='default.jpg',upload_to='profile_pics')
+    bio  = models.CharField(max_length=200,blank=True) 
 
     def __str__(self):
         return f'{self.user.username} Profile'
