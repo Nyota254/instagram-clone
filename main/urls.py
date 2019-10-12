4c26035cd2ea66e4480b80ae6fd8b88e5547b582
Nyota254 <knyota66@gmail.com>
2019-10-12 18:46:25 +0300

    add functionality for user to upload picture

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import re_path
-from .views import ImageListView,ImageDetailView
+from .views import (
+    ImageListView,
+    ImageDetailView,
+    ImageCreateView
+)
 from . import views
 
 urlpatterns = [
     re_path(r'^$',ImageListView.as_view(),name="home_view"),
-    re_path(r'^image/(?P<pk>\d+)',ImageDetailView.as_view(),name="image_detail")
+    re_path(r'^image/(?P<pk>\d+)',ImageDetailView.as_view(),name="image_detail"),
+    re_path(r'^image/new$',ImageCreateView.as_view(),name="image_upload")
 ]
--- main/views.py
+++ main/views.py
@@ -21,3 +21,17 @@ class ImageDetailView(DetailView):
     Class based view for viewing specific image with its details
     '''
     model = Image
+
+class ImageCreateView(CreateView):
+    '''
+    Class based view for addind new image
+    '''
+    model = Image
+    fields = ['image','image_name','image_caption']
+
+    def form_valid(self,form):
+        '''
+        form overide to set user who uploaded image
+        '''
+        form.instance.profile = self.request.user
+        return super().form_valid(form)
