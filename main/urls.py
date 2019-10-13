c4492cc206e23c07d920d005d566d7c96b25d8b6
Nyota254 <knyota66@gmail.com>
2019-10-13 11:54:53 +0300

    change name of home_view to index_view

--- main/urls.py
+++ main/urls.py
@@ -9,7 +9,7 @@ from .views import (
 from . import views
 
 urlpatterns = [
-    re_path(r'^$',ImageListView.as_view(),name="home_view"),
+    re_path(r'^$',ImageListView.as_view(),name="index_view"),
     re_path(r'^image/(?P<pk>\d+)$',ImageDetailView.as_view(),name="image_detail"),
     re_path(r'^image/new$',ImageCreateView.as_view(),name="image_upload"),
     re_path(r'^image/(?P<pk>\d+)/update',ImageUpdateView.as_view(),name="image_update"),
